e778e34a6b96eb204681fa118be0b3f917fed4ac
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-05-24 23:27:06 +0530

    Added new sql script

--- .openshift/action_hooks/ffs.sql
+++ .openshift/action_hooks/ffs.sql
@@ -1,15 +1,45 @@
-CREATE TABLE posts (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50),
-    body TEXT,
-    created DATETIME DEFAULT NULL,
-    modified DATETIME DEFAULT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 4.0.0
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 24, 2013 at 04:07 PM
+-- Server version: 5.5.30
+-- PHP Version: 5.4.15
 
-INSERT INTO posts (title,body,created)
-    VALUES ('The title', 'This is the post body.', NOW());
-INSERT INTO posts (title,body,created)
-    VALUES ('A title once again', 'And the post body follows.', NOW());
-INSERT INTO posts (title,body,created)
-    VALUES ('Title strikes back', 'This is really exciting! Not.', NOW());
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ffs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`) VALUES
+(2, 'udinnet');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
