224bad8e768c55465c37fce139d5018ed2570215
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-05-26 04:03:33 +0530

    Added ticketing component's MVC

---
+++ php/app/Controller/TicketsController.php
@@ -0,0 +1,100 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Tickets Controller
+ *
+ * @property Ticket $Ticket
+ */
+class TicketsController extends AppController {
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Ticket->recursive = 0;
+		$this->set('tickets', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Ticket->exists($id)) {
+			throw new NotFoundException(__('Invalid ticket'));
+		}
+		$options = array('conditions' => array('Ticket.' . $this->Ticket->primaryKey => $id));
+		$this->set('ticket', $this->Ticket->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Ticket->create();
+			if ($this->Ticket->save($this->request->data)) {
+				$this->Session->setFlash(__('The ticket has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The ticket could not be saved. Please, try again.'));
+			}
+		}
+		$users = $this->Ticket->User->find('list');
+		$this->set(compact('users'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Ticket->exists($id)) {
+			throw new NotFoundException(__('Invalid ticket'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Ticket->save($this->request->data)) {
+				$this->Session->setFlash(__('The ticket has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The ticket could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Ticket.' . $this->Ticket->primaryKey => $id));
+			$this->request->data = $this->Ticket->find('first', $options);
+		}
+		$users = $this->Ticket->User->find('list');
+		$this->set(compact('users'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Ticket->id = $id;
+		if (!$this->Ticket->exists()) {
+			throw new NotFoundException(__('Invalid ticket'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Ticket->delete()) {
+			$this->Session->setFlash(__('Ticket deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Ticket was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
