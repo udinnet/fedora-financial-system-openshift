4b449bfc67f363a5a4abbd678b2e6737afaaea71
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-06-05 23:52:58 +0530

    Initiating ACL development (DBAcl)

--- php/app/Controller/AppController.php
+++ php/app/Controller/AppController.php
@@ -33,7 +33,7 @@ App::uses('FASAuthenticate', 'Controller/Component/Auth');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-    public $components = array('DebugKit.Toolbar','Session','Auth'=>array(
+    public $components = array('Acl','DebugKit.Toolbar','Session','Auth'=>array(
         'loginRedirect'=>array('controller'=>'backend','action'=>'home'),
         'logoutRedirect'=>array('controller'=>'pages','action'=>'index'),
         'authError'=>'You cannot access that page',
