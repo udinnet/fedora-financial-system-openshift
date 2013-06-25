566f5f6156d9f4bd9d11f8ceb01b436e84aa5f82
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-06-26 03:16:28 +0530

    Added the ticket fields model and CRUD that holds the varios fields like SWAG,Media which has n-to-n relationship with Ticket model

---
+++ php/app/Model/TicketField.php
@@ -0,0 +1,70 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class TicketField extends AppModel {
+
+
+	public $displayField = 'name';
+
+
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+
+			),
+		),
+	);
+
+
+
+	public $hasMany = array(
+		'Rule' => array(
+			'className' => 'Rule',
+			'foreignKey' => 'ticket_field_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'Transaction' => array(
+			'className' => 'Transaction',
+			'foreignKey' => 'ticket_field_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+
+
+	public $hasAndBelongsToMany = array(
+		'Ticket' => array(
+			'className' => 'Ticket',
+			'joinTable' => 'tickets_ticket_fields',
+			'foreignKey' => 'ticket_field_id',
+			'associationForeignKey' => 'ticket_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		)
+	);
+
+}
