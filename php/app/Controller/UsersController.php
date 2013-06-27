295ad498f34ec3a266dd6f8e8d513648a51c8141
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-06-28 00:18:24 +0530

    Added login error message to the Bootstrapper scheme

--- php/app/Controller/UsersController.php
+++ php/app/Controller/UsersController.php
@@ -1,14 +1,18 @@
 <?php
 App::uses('AppController', 'Controller');
 
+
 class UsersController extends AppController {
+
+    public $theme = 'Bootstrap';
+
     public function login() {
         if($this->request->is('post')) {
             //echo debug($this->request->data,true,true);
             if ($this->Auth->login()) {
                 return $this->redirect($this->Auth->redirectUrl());
             } else{
-                $this->Session->setFlash('Your username password combination is incorrect');
+                $this->Session->setFlash('Your username password combination is incorrect','error_flash');
             }
         }
 
