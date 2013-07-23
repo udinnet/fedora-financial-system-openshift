c4217686b197b7852bc26e1b5b954b0f88e3e236
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-07-23 11:46:46 +0530

    Added initial Transacations controller. But this need lot of modifications

---
+++ php/app/Controller/TransactionsController.php
@@ -0,0 +1,74 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class TransactionsController extends AppController {
+
+
+	public function index() {
+		$this->Transaction->recursive = 0;
+		$this->set('transactions', $this->paginate());
+	}
+
+
+	public function view($id = null) {
+		if (!$this->Transaction->exists($id)) {
+			throw new NotFoundException(__('Invalid transaction'));
+		}
+		$options = array('conditions' => array('Transaction.' . $this->Transaction->primaryKey => $id));
+		$this->set('transaction', $this->Transaction->find('first', $options));
+	}
+
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Transaction->create();
+			if ($this->Transaction->save($this->request->data)) {
+				$this->Session->setFlash(__('The transaction has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The transaction could not be saved. Please, try again.'));
+			}
+		}
+		$accounts = $this->Transaction->Account->find('list');
+		$tickets = $this->Transaction->Ticket->find('list');
+		$ticketFields = $this->Transaction->TicketField->find('list');
+		$this->set(compact('accounts', 'tickets', 'ticketFields'));
+	}
+
+
+	public function edit($id = null) {
+		if (!$this->Transaction->exists($id)) {
+			throw new NotFoundException(__('Invalid transaction'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Transaction->save($this->request->data)) {
+				$this->Session->setFlash(__('The transaction has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The transaction could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Transaction.' . $this->Transaction->primaryKey => $id));
+			$this->request->data = $this->Transaction->find('first', $options);
+		}
+		$accounts = $this->Transaction->Account->find('list');
+		$tickets = $this->Transaction->Ticket->find('list');
+		$ticketFields = $this->Transaction->TicketField->find('list');
+		$this->set(compact('accounts', 'tickets', 'ticketFields'));
+	}
+
+
+	public function delete($id = null) {
+		$this->Transaction->id = $id;
+		if (!$this->Transaction->exists()) {
+			throw new NotFoundException(__('Invalid transaction'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Transaction->delete()) {
+			$this->Session->setFlash(__('Transaction deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Transaction was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
