f43e9d8cf4f9790c36d9002d91099e0f0c675af7
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-09-07 03:31:33 +0530

    Account report controller method been added to compatible with xml/json REST outputs

--- php/app/Controller/ReportsController.php
+++ php/app/Controller/ReportsController.php
@@ -4,9 +4,32 @@ class ReportsController extends AppController {
 
     }
 
-    public function account_report() {
+    public function account() {
+
+    }
+
+    public function account_report($account_name) {
         $this->loadModel('Transaction');
         $this->loadModel('Account');
         $this->loadModel('Ticket');
+
+        $account = $this->Account->find(
+            'first',
+            array(
+                'conditions'=>array('Account.name'=>$account_name),
+                'recursive' => -1
+            )
+        );
+        $account_id = $account['Account']['id'];
+        $transactions = $this->Transaction->find(
+            'all',
+            array(
+                'conditions'=>array('Transaction.account_id'=>$account_id),
+                'recursive' => -1
+            )
+        );
+        $this->set('transactions',$transactions);
     }
+
+
 }
