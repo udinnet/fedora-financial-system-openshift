3dff11873284d7036b6c9094738f9013e60079c1
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-09-10 02:00:23 +0530

    Router setup for initial REST reponse check for JSON/XML

--- php/app/Config/routes.php
+++ php/app/Config/routes.php
@@ -33,6 +33,9 @@
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
+    Router::mapResources('reports');
+    Router::parseExtensions();
+
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
